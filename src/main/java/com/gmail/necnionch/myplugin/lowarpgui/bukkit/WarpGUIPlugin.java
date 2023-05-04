8e689cd6d3a862d87eaa779d3398925f6f874d89
Necnion8 <necnionch@gmail.com>
2023-05-04 17:03:40 +0900

    initial commit

---
+++ src/main/java/com/gmail/necnionch/myplugin/lowarpgui/bukkit/WarpGUIPlugin.java
@@ -0,0 +1,18 @@
+package com.gmail.necnionch.myplugin.lowarpgui.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarpGUIPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
