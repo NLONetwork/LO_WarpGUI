a6d4023781556230a643c6c067e8bcdb83d7fc62
Necnion8 <necnionch@gmail.com>
2023-05-04 18:20:29 +0900

    Add: openGUIメソッドの追加

--- src/main/java/com/gmail/necnionch/myplugin/lowarpgui/bukkit/WarpGUIPlugin.java
+++ src/main/java/com/gmail/necnionch/myplugin/lowarpgui/bukkit/WarpGUIPlugin.java
@@ -1,7 +1,10 @@
 package com.gmail.necnionch.myplugin.lowarpgui.bukkit;
 
 import com.gmail.necnionch.myplugin.lowarpgui.bukkit.config.WarpConfig;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.geysermc.floodgate.api.FloodgateApi;
+import org.geysermc.floodgate.api.player.FloodgatePlayer;
 
 public final class WarpGUIPlugin extends JavaPlugin {
     private final WarpConfig warpConfig = new WarpConfig(this);
@@ -19,5 +22,25 @@ public final class WarpGUIPlugin extends JavaPlugin {
     public WarpConfig getWarpConfig() {
         return warpConfig;
     }
+
+
+    public void openGUI(Player player) {
+        FloodgatePlayer floodgatePlayer = FloodgateApi.getInstance().getPlayer(player.getUniqueId());
+        if (floodgatePlayer != null) {
+            openFloodgateGUI(floodgatePlayer);
+        } else {
+            openBukkitGUI(player);
+        }
+    }
+
+    public void openBukkitGUI(Player player) {
+
+    }
+
+    public void openFloodgateGUI(FloodgatePlayer player) {
+
+    }
+
+
 
 }
